2a3b1c0ac6af1a13eabe593ed5b5deaf3a37f595
Clay Reimann <clayreimann@yahoo-inc.com>
2015-05-26 14:19:44 -0500

    return `wrap` directly instead of a wrapped superagent
    * the peer dependencies on superagent and promise are being deprecated
    since peer dependencies in npm@3 are changing.
    * bump version to 1.0 since superagent is now >1.0
    * add tests against `then` on the wrapped superagent
    
    By returning the `wrap` function directly we can avoid the bundled
    dependency on `promise` and allow users to specify their preferred
    promise library.

--- index_test.js
+++ index_test.js
@@ -1,6 +1,6 @@
 suite('superagent-promise', function() {
   var assert  = require('assert');
-  var request = require('./');
+  var request = require('./')(require('superagent'), require('es6-promise').Promise);
   var http    = require('http');
   var debug   = require('debug')('index_test');
 
@@ -11,22 +11,22 @@ suite('superagent-promise', function() {
   setup(function(done) {
     server = http.createServer(function(req, res) {
       if (/success$/.test(req.url)) {
-        debug("Responding with 200");
+        debug('Responding with 200');
         res.writeHead(200, {
           'Content-Length': successBody.length,
           'Content-Type': 'text/plain'
         });
         res.end(successBody);
       } else if(/NotFound$/.test(req.url)) {
-        debug("Responding with 404");
+        debug('Responding with 404');
         res.writeHead(404, {
           'Content-Length': errorBody.length,
           'Content-Type': 'text/plain'
         });
         res.end(errorBody);
       } else if(/error$/.test(req.url)) {
-        debug("Responding with 200, but mismatching Content-Length");
-        res.writeHead(404, {
+        debug('Responding with 200, but mismatching Content-Length');
+        res.writeHead(200, {
           'Content-Length': successBody.length - 2,
           'Content-Type': 'text/plain'
         });
@@ -34,51 +34,103 @@ suite('superagent-promise', function() {
       }
     });
 
-    server.listen(0, done);
+    server.listen(0, function() {
+      debug('listen');
+      done();
+    });
   });
 
   teardown(function(done) {
-    server.close(done);
-  });
-
-  test('issue request', function() {
-    var addr = server.address();
-    var url = 'http://' + addr.address + ':' + addr.port + "/success";
-
-    return request('GET', url).end().then(function(res) {
-      assert.equal(res.text, successBody);
-    });
-  });
-
-  test('issue request with .get', function() {
-    var addr = server.address();
-    var url = 'http://' + addr.address + ':' + addr.port + "/success";
-
-    return request.get(url).end().then(function(res) {
-      assert.equal(res.text, successBody);
-    });
-  });
-
-  test('issue 404 request', function() {
-    var addr = server.address();
-    var url = 'http://' + addr.address + ':' + addr.port + "/NotFound";
-
-    return request('GET', url).end().then(function(res) {
-      assert.ok(!res.ok);
-      assert.equal(res.text, errorBody);
-    });
-  });
-
-  test('test error', function(done) {
-    var addr = server.address();
-    var url = 'http://' + addr.address + ':' + addr.port + "/error";
-
-    request('GET', url).end().then(function(res) {
-      assert.ok(false);
-      done();
-    }, function(err) {
-      assert.ok(err);
+    server.close(function() {
+      debug('teardown');
       done();
     });
+    server = undefined;
   });
+
+  suite('#end', function() {
+    test('issue request', function(done) {
+      var addr = server.address();
+      var url = 'http://' + addr.address + ':' + addr.port + '/success';
+
+      request('GET', url).end().then(function(res) {
+        assert.equal(res.text, successBody);
+      }).then(done).catch(done);
+    });
+
+    test('issue request with .get', function(done) {
+      var addr = server.address();
+      var url = 'http://' + addr.address + ':' + addr.port + '/success';
+
+      request.get(url).end().then(function(res) {
+        assert.equal(res.text, successBody);
+      }).then(done).catch(done);
+    });
+
+    test('issue 404 request', function(done) {
+      var addr = server.address();
+      var url = 'http://' + addr.address + ':' + addr.port + '/NotFound';
+
+      request('GET', url).end().then(undefined, function(err) {
+        assert.equal(err.status, 404)
+        assert.equal(err.response.text, errorBody);
+
+      }).then(done).catch(done);
+    });
+
+    test('test error', function(done) {
+      var addr = server.address();
+      var url = 'http://' + addr.address + ':' + addr.port + '/error';
+
+      request('GET', url).end().then(function(res) {
+        done(new Error('error should not should not succeed'));
+
+      }, function(err) {
+        assert.ok(err);
+      }).then(done).catch(done);
+    });
+  });
+
+  suite('#then', function() {
+    test('issue request', function(done) {
+      var addr = server.address();
+      var url = 'http://' + addr.address + ':' + addr.port + '/success';
+
+      request('GET', url).then(function(res) {
+        assert.equal(res.text, successBody);
+      }).then(done).catch(done);
+    });
+
+    test('issue request with .get', function(done) {
+      var addr = server.address();
+      var url = 'http://' + addr.address + ':' + addr.port + '/success';
+
+      request.get(url).then(function(res) {
+        assert.equal(res.text, successBody);
+      }).then(done).catch(done);
+    });
+
+    test('issue 404 request', function(done) {
+      var addr = server.address();
+      var url = 'http://' + addr.address + ':' + addr.port + '/NotFound';
+
+      request('GET', url).then(undefined, function(err) {
+        assert.equal(err.status, 404)
+        assert.equal(err.response.text, errorBody);
+
+      }).then(done).catch(done);
+    });
+
+    test('test error', function(done) {
+      var addr = server.address();
+      var url = 'http://' + addr.address + ':' + addr.port + '/error';
+
+      request('GET', url).then(function(res) {
+        done(new Error('error should not should not succeed'));
+
+      }, function(err) {
+        assert.ok(err);
+      }).then(done).catch(done);
+    });
+  })
 });
